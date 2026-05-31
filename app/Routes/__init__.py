92e7fdad1376cb28ddb1426176a53995d807adf6
maleksabbah <maleksabbah04@gmail.com>
2026-05-31 09:49:02 +0100

    fix: export file_router from Routes init

--- app/Routes/__init__.py
+++ app/Routes/__init__.py
@@ -1,9 +1,11 @@
 from app.Routes.AuthRoutes import router as auth_router
 from app.Routes.UserRoutes import router as user_router
 from app.Routes.JobRoutes import router as job_router
+from app.Routes.FileRoutes import router as file_router
 
 __all__ = [
     "auth_router",
     "user_router",
     "job_router",
-  ]
+    "file_router",
+]
